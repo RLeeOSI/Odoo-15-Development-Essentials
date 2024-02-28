19049a4a857c033acc9c401c4d34d7154560d203
RLeeOSI <51208020+RLeeOSI@users.noreply.github.com>
2024-02-27 16:58:10 -0800

    Add module category and icon

--- work17/library/library_app/__manifest__.py
+++ work17/library/library_app/__manifest__.py
@@ -2,6 +2,7 @@
     "name": "Library Management",
     "summary": "Manage library catalog and book lending.",
     "author": "Daniel Reis",
+    "category": "Services/Library",
     "license": "AGPL-3",
     "website": "https://github.com/PacktPublishing/Odoo-17-Development-Essentials",
     "version": "17.0.1.0.0",
