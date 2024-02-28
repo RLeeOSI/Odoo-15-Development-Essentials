b70a89b5365e6cb0213bf3f30e353ea12afe0377
RLeeOSI <51208020+RLeeOSI@users.noreply.github.com>
2024-02-27 17:00:45 -0800

    Add a menuitem

--- work17/library/library_app/__manifest__.py
+++ work17/library/library_app/__manifest__.py
@@ -7,5 +7,8 @@
     "website": "https://github.com/PacktPublishing/Odoo-17-Development-Essentials",
     "version": "17.0.1.0.0",
     "depends": ["base"],
+        "data": [
+        "views/library_menu.xml",
+    ],
     "application": True,
 }
